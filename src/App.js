948600ec0ffd631c92aafdbf85cc3c044c635ce6
Pedro H. Silva <pedroh_silva@outlook.com>
2020-09-05 10:10:00 -0300

    refactored project, challenge completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import {
   SafeAreaView,
@@ -11,45 +12,88 @@ import {
 } from "react-native";
 
 export default function App() {
-  async function handleLikeRepository(id) {
-    // Implement "Like Repository" functionality
+  const [repositories, setRepositories] = useState([])
+
+  const getRepositories = async () => {
+    try {
+      const response = await api.get('repositories')
+      if (response.status !== 200) throw new Error('could not get the data')
+      const { data } = response
+      setRepositories(data)
+    } catch (error) {
+      console.log(error.message)
+    }
   }
+
+  useEffect(() => {
+    getRepositories()
+  }, [])
+
+  const getIndexProjectInRepository = (id, repositories) =>
+    repositories.findIndex(({ id: uuid }) => uuid === id)
+
+  async function handleLikeRepository(id) {
+    //try {
+      const { data, status } = await api.post(`repositories/${id}/like`)
+      //if (status !== 201) throw new Error('repository not found')
+      const projectIndex = getIndexProjectInRepository(id, repositories)
+      const newRepositories = repositories.map(repo => repo)
+      newRepositories[projectIndex] = data
+      setRepositories(newRepositories)
+    //} catch (error) {
+      //console.log(error.message)
+    //}
+  }
+
+  const RepoItem = (({ id, url, title, techs, likes }) => (
+    < View style={styles.repositoryContainer}>
+
+      <Text style={styles.repository}>{title}</Text>
+
+      <View style={styles.techsContainer}>
+        {techs.map(tech => <Text style={styles.tech} key={tech}>
+          {tech}
+        </Text>)}
+      </View>
+
+      <View style={styles.likesContainer}>
+        <Text
+          style={styles.likeText}
+          // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
+          testID={`repository-likes-${id}`}
+        >
+          {`${likes} curtidas`}
+        </Text>
+      </View>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => handleLikeRepository(id)}
+        // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
+        testID={`like-button-${id}`}
+      >
+        <Text style={styles.buttonText}>Curtir</Text>
+      </TouchableOpacity>
+    </View>
+  ))
+
+  const renderItem = ({ item }) =>
+    <RepoItem
+      id={item.id}
+      title={item.title}
+      techs={item.techs}
+      likes={item.likes}
+    />
 
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <SafeAreaView style={styles.container}>
-        <View style={styles.repositoryContainer}>
-          <Text style={styles.repository}>Repository 1</Text>
-
-          <View style={styles.techsContainer}>
-            <Text style={styles.tech}>
-              ReactJS
-            </Text>
-            <Text style={styles.tech}>
-              Node.js
-            </Text>
-          </View>
-
-          <View style={styles.likesContainer}>
-            <Text
-              style={styles.likeText}
-              // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-              testID={`repository-likes-1`}
-            >
-              3 curtidas
-            </Text>
-          </View>
-
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => handleLikeRepository(1)}
-            // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-            testID={`like-button-1`}
-          >
-            <Text style={styles.buttonText}>Curtir</Text>
-          </TouchableOpacity>
-        </View>
+        <FlatList
+          data={repositories}
+          keyExtractor={item => item.id}
+          renderItem={renderItem}
+        />
       </SafeAreaView>
     </>
   );
